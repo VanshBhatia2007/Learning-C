60edff081d212d5c1ca72be4b78637fd7b499cc1
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-12 18:11:02 +0530

    variables

---
+++ Topics/variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int age = 20;
+    float pi = 3.14;
+    char star = '*';
+    printf("my age is %d \n", age);
+    printf("%f %c", pi, star);
+    return 0;
+}
