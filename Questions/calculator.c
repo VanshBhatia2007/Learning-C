97404945eeee10ca460892ef5bde3915cd9a8ff6
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-09-16 19:12:39 +0530

    calculator

---
+++ Questions/calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    char op;
+    float no1,no2;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c",&op);
+    switch(op){
+        case '+':
+            printf("Enter two numbers: ");
+            scanf("%f %f",&no1,&no2);
+            printf("%.1f + %.1f = %.1f",no1,no2,no1+no2);
+            break;
+        case '-':
+            printf("Enter two numbers: ");
+            scanf("%f %f",&no1,&no2);
+            printf("%.1f - %.1f = %.1f",no1,no2,no1-no2);
+            break;
+        case '*':
+            printf("Enter two numbers: ");
+            scanf("%f %f",&no1,&no2);
+            printf("%.1f * %.1f = %.1f",no1,no2,no1*no2);
+            break;
+        case '/':
+            printf("Enter two numbers: ");
+            scanf("%f %f",&no1,&no2);
+            if(no2!=0)
+                printf("%.1f / %.1f = %.1f",no1,no2,no1/no2);
+            else
+                printf("Error! Division by zero.");
+            break;
+        default:
+            printf("Error! operator is not correct");
+            break;    }
+}
